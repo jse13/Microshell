4355ccd7da3ffa914b3992482cc1083d18ca22e4
Julian Engel <me@julianengel.com>
2016-09-06 12:56:50 -0400

    Added main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<string.h>
+#include<stdio.h>
+
+int main(int argc, char* argv[]) {
+
+//TODO: Determine whether this shell is being run in interactive mode or batch mode
+
+}
+
+//TODO: Controlling function for interactive mode
+void interactive() {
+
+}
+
+//TODO: Controlling function for batch mode
+void batch() {
+
+	//TODO: parse list of commands from file
+	
+	//TODO: get the command
+	//TODO: echo the command before running it
+	//TODO: determine if the command is a ; separated list
+	//TODO: runn command
+}
